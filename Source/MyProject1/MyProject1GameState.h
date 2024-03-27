20967421274a266316daf98f67f0f8e336e01fc1
HryhoriiDyadynchuk <hryhoriidyadynchuk2010@gmail.com>
2024-03-27 17:50:13 +0200

    .

---
+++ Source/MyProject1/MyProject1GameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "MyProject1GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT1_API AMyProject1GameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MyProject1/MyProject1PlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "MyProject1PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT1_API AMyProject1PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
